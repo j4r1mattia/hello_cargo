b989f457682d57954e15522d52f9fe97cff0799b
j4r1 <jari.mattia@gmail.com>
2023-04-10 00:37:43 +0200

    play with structs

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,37 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 10;
-        }
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
-    println!("result = {result}");
+    let sqr = Rectangle::square(5);
 
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-
-        count += 1;
+    println!("The area of rectangle is {}", rect1.area());
+    if rect1.can_hold(&sqr) {
+        println!("Square area is less than rectangle area");
     }
-    println!("End count = {count}");
-
-
-    let mut number = 5;
-
-    while number != 0 {
-        println!("number = {number}");
-        number -= 1;
-    }
-    println!("ENDED While loop!");
-
-    let a = [10,20,30,40,50];
-
-    for element in a {
-        println!("{element}");
-    }
-    
-    for element in 1..=10  {
-        println!("{element}");
-    }
+    dbg!(&rect1);
 }
