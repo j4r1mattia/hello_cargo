07e68e7149d5968b48e998d59ab77467ca2d3306
j4r1 <jari.mattia@gmail.com>
2023-04-09 22:59:18 +0200

    playing with loops

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 fn main() {
+    let mut counter = 0;
 
-    let number = 3;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 10;
+        }
+    };
+    println!("result = {result}");
 
-    if number < 5 {
-        println!("condition is true");
-    } else {
-        println!("condition is false");
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
     }
+    println!("End count = {count}");
 
 
-    let condition = true;
+    let mut number = 5;
 
-    let result = if condition {5} else {6};
-    println!("result is {result}");
+    while number != 0 {
+        println!("number = {number}");
+        number -= 1;
+    }
+    println!("ENDED While loop!");
 
+    let a = [10,20,30,40,50];
+
+    for element in a {
+        println!("{element}");
+    }
+    
+    for element in 1..=10  {
+        println!("{element}");
+    }
 }
